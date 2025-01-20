9ba2c649f511835983e8c3aac7cae28ff9b8afe3
allarkpet <fishygayming@gmail.com>
2025-01-20 18:19:31 +0100

    {feat} file.txt, pseudocode of get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -12,7 +12,37 @@
 
 #include "get_next_line.h"
 
+//function to read_text
+void read_text(int fd)
+{
+    // variable char *buff_read ((static stash?))
+
+    //read (fd, buff_read, BUFFER_SIZE)
+    //handle error when read return -1
+    // stash = malloc (strlen stash + BUFFER_SIZE)
+    // strcat stash, buff_read
+}
+
 char *get_next_line.c(int fd)
 {
+//variables. static *stash, char *returned_array.
+static char *stash;
+char    *returned_array;
 
+//assign
+stash = VOID;
+//fd = open assign value of fd with open function
+// if fd < 0) return error
+
+// make a if condition to handle errors or weird inputs
+
+// while loop (condition = read the content of stash to see if there is any \n?) 
+    {
+
+    //stash = read_text function to read the text until the buffer size is reached
+    //if (function is_next_line(static char *stash) != 0
+        //break
+    }
+    
 }
+
--- get_next_line.h
+++ get_next_line.h
@@ -10,5 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+# endif
+
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 char  *get_next_line(int fd);
