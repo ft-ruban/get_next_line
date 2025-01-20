fde457fe2721e55044be4d55c5d783e44b34ecb3
allarkpet <fishygayming@gmail.com>
2025-01-20 20:37:52 +0100

    {update} get_next_line (still not finished)

--- get_next_line.c
+++ get_next_line.c
@@ -13,36 +13,92 @@
 #include "get_next_line.h"
 
 //function to read_text
-void read_text(int fd)
-{
-    // variable char *buff_read ((static stash?))
 
-    //read (fd, buff_read, BUFFER_SIZE)
-    //handle error when read return -1
-    // stash = malloc (strlen stash + BUFFER_SIZE)
-    // strcat stash, buff_read
+char *write_and_save()
+{
+    int  i;
+    int  j;
+    char *buff;
+    char *return_value;
+
+    i = 0;
+    j = 0;
+    while (stash[i] != '\n' || stash[i] != 0)
+        i++;
+    return_value = malloc ((i + 1) * (sizeof(char)))
+    if (!return_value)
+        return (NULL);
+    //function to copy content of stash into return value
+
+    //create a function to handle that?
+    if stash[i] == '\n'
+    {
+        j = i;
+        while (stash[i] != 0)
+            i++;
+        buff = malloc((i - j + 1)*(sizeof(char)));
+        if (!= buff)
+            return (NULL);
+        strcpy(buff, stash[j]);
+        free stash;
+        stash = malloc((i - j +1)*(sizeof(char)));
+        if (!stash)
+            return (NULL);
+        strcpy(stash,buff);
+    }
+    else 
+        free stash;
+    return(return_value);
 }
 
-char *get_next_line.c(int fd)
+int is_next_line()
+{
+    if (strchr(stash, "\n"))
+        return (1);
+    else 
+    return (0);
+}
+
+char *read_text(int fd)
+{
+    // variable char *buff_read ((static stash?))
+    char *buff_read;
+
+    buff_read = malloc ((BUFFER_SIZE + 1) * (sizeof(char)))
+    if (!buff_read)
+        return (NULL);
+    //read (fd, buff_read, BUFFER_SIZE)
+    if (read (fd, buff_read, BUFFER_SIZE) == -1)
+    //handle error when read return -1
+        return (NULL);
+    // stash = malloc (strlen stash + BUFFER_SIZE)
+    stash = malloc (strlen (stash) + BUFFER_SIZE);
+    if (!stash)
+        return (NULL);
+    // strcat stash, buff_read
+    strcat (stash, buff_read);
+    free (buff_read);
+    return (stash);
+}
+
+char *get_next_line(int fd)
 {
 //variables. static *stash, char *returned_array.
-static char *stash;
-char    *returned_array;
+    static char *stash;
+    char    *returned_array;
 
 //assign
-stash = VOID;
-//fd = open assign value of fd with open function
-// if fd < 0) return error
-
-// make a if condition to handle errors or weird inputs
-
-// while loop (condition = read the content of stash to see if there is any \n?) 
+    fd = open ("file.txt", O_RDONLY | O_CREAT);
+    if (fd < 0)
+        return (NULL);
+// while loop (condition = read the content of stash to see if there is any \n?)
+    while
     {
-
-    //stash = read_text function to read the text until the buffer size is reached
-    //if (function is_next_line(static char *stash) != 0
-        //break
+        //function to read the text until the buffer size is reached
+        stash = read_text(fd, stash);
+        if (is_next_line(stash) != 0)
+            break;
     }
-    
+    return (returned_array = write_and_save ());    
 }
 
