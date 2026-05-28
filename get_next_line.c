f8e0019eeea8a2f6f3cc87e91ca2b97f272f0fee
Lea Devoudelle-Royer <ldevoude@z3r3p6.42lyon.fr>
2026-05-28 09:36:31 +0200

    update(get_next_line*): updated to the version I released at school before knowing about git remote.

--- get_next_line.c
+++ get_next_line.c
@@ -3,123 +3,147 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldevoude <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ldevoude <ldevoude@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/16 15:09:54 by ldevoude          #+#    #+#             */
-/*   Updated: 2025/01/16 15:12:30 by ldevoude         ###   ########.fr       */
+/*   Created: 2025/01/29 13:11:17 by ldevoude          #+#    #+#             */
+/*   Updated: 2025/02/02 13:16:46 by ldevoude         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
 #include "get_next_line.h"
-#include <stdio.h>
-//function to read_tex
-static char *stash;
 
-char *write_and_save()
+// clean the line until it meet the \n to it will free sbuff if the file is
+// finished, else it will return what is necessary to keep for the next
+// use of get_next_line.c
+char	*newline_cleaner(char *sbuff)
 {
-    int  i;
-    int  j;
-    char *buff;
-    char *return_value;
+	int		i;
+	int		j;
+	char	*temp;
 
-    i = 0;
-    while (stash[i] != '\n' && stash[i] != 0)
-        i++;
-    if (stash[i] == 0)
-    {
-        return_value = malloc ((i + 1) * (sizeof(char)));
-        ft_strlcpy(return_value, stash, BUFFER_SIZE + 1);
-        free (stash);
-        return (return_value);
-    }
-    else
-    j = i;
-    buff = malloc((i + 1)*(sizeof(char)));
-    if (!buff)
-        return (NULL);
-    //function to copy content of stash into return value
-    //create a function to handle that?
-    if (stash[i] == '\n')
-    {
-        while (stash[i] != 0)
-            i++;
-        if (!buff)
-            return (NULL);
-        strcpy(buff, stash); //find a way to truncate stash
-        free (stash);
-        stash = malloc((i - j +1)*(sizeof(char)));
-        if (!stash)
-            return (NULL);
-        strcpy(stash,buff);
-    }
-    else 
-       free (stash);
-    //printf("buff = %s\n",return_value);
-    //strcpy(buff,return_value);
-    //printf("return_value : %s\n",return_value);
-    return("a");
+	i = 0;
+	j = 0;
+	while (sbuff[i] != 0 && sbuff[i] != '\n')
+		i++;
+	if (!sbuff[i])
+	{
+		free(sbuff);
+		return (NULL);
+	}
+	temp = ft_calloc((ft_strlen(sbuff) - i + 1), sizeof(char));
+	if (!temp)
+		return (free(sbuff), NULL);
+	i++;
+	while (sbuff[i] != 0)
+	{
+		temp[j] = sbuff[i];
+		i++;
+		j++;
+	}
+	free(sbuff);
+	return (temp);
+}
+// it transfer the content of sbuff into the string that I plan to return
+// in the end of the get_next
+
+char	*fill_returned_line(char *sbuff, char *returned_line)
+{
+	int	i;
+
+	if (!sbuff || !*sbuff)
+		return (NULL);
+	i = 0;
+	while (sbuff[i] != 0 && sbuff[i] != '\n')
+		i++;
+	returned_line = ft_calloc((i + 2), (sizeof(char)));
+	if (!returned_line)
+		return (NULL);
+	i = 0;
+	while (sbuff[i] != 0 && sbuff[i] != '\n')
+	{
+		returned_line[i] = sbuff[i];
+		i++;
+	}
+	if (sbuff[i] && sbuff[i] == '\n')
+		returned_line[i] = '\n';
+	return (returned_line);
+}
+// explicit name, it merge two strings (sbuff + buff) then it return the merged
+// version
+
+char	*merge_then_free(char *dest, char *src)
+{
+	char	*new_str;
+
+	new_str = ft_strjoin(dest, src);
+	if (dest)
+		free(dest);
+	if (!new_str)
+		return (NULL);
+	return (new_str);
+}
+// That function does read the content of the file associated by the FD
+// and would continue to do so until the \n is met! then we go to merge
+// then free and we return sbuff that should
+// contain the old leftover + the new line
+
+char	*read_content_then_stock(int fd, char *sbuff)
+{
+	char	*buff;
+	int		bytes_len;
+
+	if (!sbuff)
+		sbuff = ft_calloc(1, 1);
+	buff = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (!buff)
+		return (free(sbuff), NULL);
+	bytes_len = 1;
+	while (bytes_len > 0)
+	{
+		bytes_len = read(fd, buff, BUFFER_SIZE);
+		if (bytes_len == -1)
+		{
+			free (sbuff);
+			return (free(buff), NULL);
+		}
+		buff[bytes_len] = 0;
+		sbuff = merge_then_free(sbuff, buff);
+		if (!sbuff)
+			return (free(buff), NULL);
+		if (ft_strchr(buff, '\n'))
+			break ;
+	}
+	return (free(buff), sbuff);
 }
 
-size_t read_text(int fd)
+// start of the function,
+// it also check in cases of errors init variables tht would be usefull later
+// sbuff and returned_line would get the return of the different function
+// and in the end sbuff would be cleaned to hold the perfect content
+// then we return returned_line that does contain the whole line.
+
+char	*get_next_line(int fd)
 {
-    // variable char *buff_read ((static stash?))
-    char *buff_read;
-    long i;
+	static char	*sbuff;
+	char		*returned_line;
 
-    i = 0;
-    buff_read = malloc ((BUFFER_SIZE + 1) * (sizeof(char)));
-    if (!buff_read)
-        return (NULL);
-    //read (fd, buff_read, BUFFER_SIZE)
-    i = read (fd, buff_read, BUFFER_SIZE)
-    if (i = -1)
-        return (NULL);
-    if (i != BUFFER_SIZE)
-    {
-        stash = malloc ((i) * (sizeof(char)));
-
-    }
-    stash = malloc (strlen (stash) + BUFFER_SIZE);
-    if (!stash)
-        return (NULL);
-    // strcat stash, buff_read
-    printf("buff_read : %s\n", buff_read);
-    strcat (stash, buff_read);
-    free (buff_read);
-    printf("stash in read_text : %s\n", stash);
-    return (stash);
+	returned_line = (NULL);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+	{
+		free(sbuff);
+		sbuff = NULL;
+		return (NULL);
+	}
+	sbuff = read_content_then_stock(fd, sbuff);
+	if (!sbuff)
+		return (NULL);
+	returned_line = fill_returned_line(sbuff, returned_line);
+	if (!returned_line)
+	{
+		free(sbuff);
+		sbuff = (NULL);
+		return (NULL);
+	}
+	sbuff = newline_cleaner(sbuff);
+	return (returned_line);
 }
-
-char *get_next_line(int fd)
-{
-    char    *returned_array;
-    size_t len;
-    int b;
-    //fd = open ("file.txt", O_RDONLY | O_CREAT);
-    len = 0;
-    stash = malloc ((BUFFER_SIZE + 1) * (sizeof(char)));
-    if (!stash)
-        return (NULL);
-    if (fd < 0)
-        return (NULL);
-        while (!is_next_line())
-        {
-        stash = read_text(fd);
-        if (!stash)
-        return (NULL);
-        printf("stash in get_next_line : %s\n", stash);
-        if (is_next_line(stash))
-        {
-            break;
-        }
-        returned_array = malloc ((strlen returned_array + BUFFER_SIZE) * (sizeof(char)));
-        if (!returned_array)
-            return (NULL);
-        //function to assembly the actual content of returned array with the actual content of stash
-        len += BUFFER_SIZE;
-        }
-    //have to handle the case if it stopped because of a newline or because there was nothing left
-    return (returned_array = write_and_save ());    
-}
-
